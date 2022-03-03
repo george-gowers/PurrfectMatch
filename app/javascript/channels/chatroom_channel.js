3c26b1efe0262d82c3634f44dd95acbf94bf6c6d
Paula Garcia Llopis <paulagllopis@gmail.com>
2022-03-03 20:22:23 +0100

    pictures and grid

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,16 +1,30 @@
 import consumer from "./consumer";
 
-const initChatroomCable = () => {
+// const initChatroomCable = () => {
+//   const messagesContainer = document.getElementById('messages');
+//   if (messagesContainer) {
+//     const id = messagesContainer.dataset.chatroomId;
+
+//     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
+//       received(data) {
+//         console.log("hey");
+//         messagesContainer.insertAdjacentHTML("beforeend", data);
+//       },
+//     });
+//   }
+// }
+
+// export { initChatroomCable };
+
+document.addEventListener('turbolinks:load', () => {
   const messagesContainer = document.getElementById('messages');
   if (messagesContainer) {
     const id = messagesContainer.dataset.chatroomId;
 
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
       received(data) {
-        messagesContainer.insertAdjacentHTML("beforeend", data)
-      },
+        messagesContainer.insertAdjacentHTML('beforeend', data); // called when data is broadcast in the cable
+      }
     });
   }
-}
-
-export { initChatroomCable };
+});
