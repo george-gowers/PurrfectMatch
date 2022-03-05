a1aab894cb992d00bdb5e03a9f927baee494fb4f
George Gowers <georgegowers@gmail.com>
2022-03-05 16:59:08 +0100

    dsd

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,20 +1,20 @@
 import consumer from "./consumer";
 
-// const initChatroomCable = () => {
-//   const messagesContainer = document.getElementById('messages');
-//   if (messagesContainer) {
-//     const id = messagesContainer.dataset.chatroomId;
+const initChatroomCable = () => {
+  const messagesContainer = document.getElementById('messages');
+  if (messagesContainer) {
+    const id = messagesContainer.dataset.chatroomId;
 
-//     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
-//       received(data) {
-//         console.log("hey");
-//         messagesContainer.insertAdjacentHTML("beforeend", data);
-//       },
-//     });
-//   }
-// }
+    consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
+      received(data) {
+        console.log("hey");
+        messagesContainer.insertAdjacentHTML("beforeend", data);
+      },
+    });
+  }
+}
 
-// export { initChatroomCable };
+export { initChatroomCable };
 
 document.addEventListener('turbolinks:load', () => {
   const messagesContainer = document.getElementById('messages');
